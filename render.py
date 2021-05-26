9d16e6d6984269e3e7f6675222ae8ef850ba2eda
Colm Britton <colmjude@gmail.com>
2021-05-26 15:56:04 +0100

    move map from leaflet-spike to /map

--- render.py
+++ render.py
@@ -7,13 +7,23 @@ from bin.utils import render, read_in_json
 
 env = setup_jinja()
 env.globals["includeAutocomplete"] = True
-map_template = env.get_template("brownfield-land-map.html")
 
-data = read_in_json("data/organisation_boundary_data.json")
+
+def render_bf_map():
+    bf_map_template = env.get_template("brownfield-land-map.html")
+    data = read_in_json("data/organisation_boundary_data.json")
+    render("national-map-of-brownfield-land.html", bf_map_template, data=data)
+
+
+def render_national_map():
+    map_template = env.get_template("national-map.html")
+    render("index.html", map_template)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "--local":
         env.globals["staticPath"] = "/static"
         env.globals["urlPath"] = ""
 
-    render("index.html", map_template, data=data)
+    render_bf_map()
+    render_national_map()
