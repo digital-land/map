ae0638789c41c5f5a0039f1c0c1fa2622fab58e2
Colm Britton <colmjude@gmail.com>
2021-07-07 15:51:50 +0100

    Add key to the layer items

--- render.py
+++ render.py
@@ -9,6 +9,15 @@ env = setup_jinja()
 env.globals["includeAutocomplete"] = True
 
 
+def hex_to_rgb_string(hex):
+    h = hex.lstrip("#")
+    rgb = tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
+    return f"{rgb[0]},{rgb[1]},{rgb[2]}"
+
+
+env.filters["hex_to_rgb"] = hex_to_rgb_string
+
+
 def render_bf_map():
     bf_map_template = env.get_template("brownfield-land-map.html")
     data = read_in_json("data/organisation_boundary_data.json")
@@ -21,24 +30,20 @@ def render_national_map():
         {
             "dataset": "local-authority-district",
             "label": "Local authority districts",
-            "paint_options": {
-              "colour": "#0b0c0c",
-              "opacity": "0.1",
-              "weight": 5
-            }
+            "paint_options": {"colour": "#0b0c0c", "opacity": "0.1", "weight": 5},
         },
         {
             "dataset": "conservation-area",
             "label": "Conservation areas",
             "paint_options": {
-              "colour": "#78AA00",
-            }
+                "colour": "#78AA00",
+            },
         },
         {
             "dataset": "brownfield-land",
             "label": "Brownfield land",
             "paint_options": {
-              "colour": "#745729",
+                "colour": "#745729",
             },
             "type": "point",
         },
@@ -46,72 +51,64 @@ def render_national_map():
             "dataset": "parish",
             "label": "Parishes",
             "paint_options": {
-              "colour": "#5694ca",
-            }
+                "colour": "#5694ca",
+            },
         },
         {
             "dataset": "heritage-coast",
             "label": "Heritage coast",
             "paint_options": {
-              "colour": "#912b88",
-            }
+                "colour": "#912b88",
+            },
         },
         {
             "dataset": "area-of-outstanding-natural-beauty",
             "label": "Areas of outstanding natural beauty",
             "paint_options": {
-              "colour": "#d53880",
-            }
+                "colour": "#d53880",
+            },
         },
         {
             "dataset": "ancient-woodland",
             "label": "Ancient woodland",
-            "paint_options": {
-              "colour": "#00703c",
-              "opacity": 0.2
-            }
+            "paint_options": {"colour": "#00703c", "opacity": 0.2},
         },
         {
             "dataset": "green-belt",
-            "label": "Green belt", 
-            "paint_options": {
-                "colour": "#85994b"
-            }
+            "label": "Green belt",
+            "paint_options": {"colour": "#85994b"},
         },
         {
             "dataset": "world-heritage-site",
             "label": "World heritage site",
             "paint_options": {
-              "colour": "#012169",
-            }
+                "colour": "#012169",
+            },
         },
         {
             "dataset": "battlefield",
             "label": "Battlefields",
-            "paint_options": {
-                "colour": "#4d2942",
-                "opacity": 0.2
-            }
+            "paint_options": {"colour": "#4d2942", "opacity": 0.2},
         },
         {
             "dataset": "park-and-garden",
             "label": "Parks and gardens",
             "paint_options": {
-              "colour": "#0EB951",
-            }
+                "colour": "#0EB951",
+            },
         },
         {
             "dataset": "protected-wreck-site",
             "label": "Protected wreck sites",
             "paint_options": {
-              "colour": "#0b0c0c",
-            }
+                "colour": "#0b0c0c",
+            },
         },
         {
             "dataset": "listed-building",
             "label": "Listed buildings",
             "paint_options": {
-              "colour": "#F9C744",
+                "colour": "#F9C744",
             },
             "type": "point",
         },
@@ -119,50 +116,48 @@ def render_national_map():
             "dataset": "special-area-of-conservation",
             "label": "Special areas of conservation",
             "paint_options": {
-              "colour": "#7A8705",
-            }
+                "colour": "#7A8705",
+            },
         },
         {
             "dataset": "scheduled-monument",
             "label": "Scheduled monuments",
             "paint_options": {
-              "colour": "#0F9CDA",
-            }
+                "colour": "#0F9CDA",
+            },
         },
         {
             "dataset": "heritage-at-risk",
             "label": "Heritage at risk",
             "paint_options": {
-              "colour": "#8D73AF",
-            }
+                "colour": "#8D73AF",
+            },
         },
         {
             "dataset": "certificate-of-immunity",
             "label": "Certificate of immunity",
             "paint_options": {
-              "colour": "#D8760D",
-            }
+                "colour": "#D8760D",
+            },
         },
         {
             "dataset": "building-preservation-notice",
             "label": "Building preservation notices",
             "paint_options": {
-              "colour": "#f944c7",
-            }
+                "colour": "#f944c7",
+            },
         },
         {
             "dataset": "ramsar",
-            "label": "Ramsar", 
-            "paint_options": {
-                "colour": "#7fcdff"
-            }
+            "label": "Ramsar",
+            "paint_options": {"colour": "#7fcdff"},
         },
         {
             "dataset": "site-of-special-scientific-interest",
             "label": "Sites of special scientific interest",
             "paint_options": {
-              "colour": "#308fac",
-            }
+                "colour": "#308fac",
+            },
         },
     ]
     render(
